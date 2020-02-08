d88389e3b48afeae8bc09d35644b221215588e50
christiandahlberg <ch7040da-s@student.lu.se>
2020-02-08 15:22:07 +0100

    Added multiple features

--- budget.py
+++ budget.py
@@ -13,13 +13,15 @@
 # ***** COMMANDS *****
 # ... $ python3 budget.py --helpme                      (Shows all commands)
 # ... $ python3 budget.py init                          (Initiates a new budget with following commands)
-# ... $ python3 budget.py new <labeL>                   (Adds a new label to current budget)
-# ... $ python3 budget.py del <label>                   (Removes label from current budget)
+# ... $ python3 budget.py new <category> <label>        (Adds a new label to current budget)
+# ... $ python3 budget.py del <category> <label>        (Removes label from current budget)
 # ... $ python3 budget.py show                          (Shows budget as a pandas dataframe)
 # ... $ python3 budget.py labels                        (Shos labels, ie Food, Snacks, Cinema, Taxis, Clothing)
 # ... $ python3 budget.py add <amount> <label>          (Adds <amount> into <label> for current month/date/year)
 # ... $ python3 budget.py sub <amount> <label>          (Subtracts <amount> from <label> for current month/date/year)
 # ... $ python3 budget.py undo                          (Undos the last used command)
+# TODO: Add a way to 'activate' your budget for data manipulation
+# TODO: Add "manage" argument to change name of labels (ie $ python3 budget.py manage Hone Home)
 
 
 # ***** FUTURE DEVELOPMENTS *****
@@ -27,25 +29,38 @@
 # TODO Function: refactor current amount on x label for y date
 # TODO Add an Exception Handler for custom exceptions (see Exception hook below)
 # TODO Extract json to csv (view budget as table)
+# TODO Configure threshold of each label (what to spend each month)
+# TODO Get profits/losses, summaries, etc.
+# TODO Show graphs and plots (numpy, matplotlib)
+# TODO Add support for different currencies
+# TODO Add "$ python3 budget.py download" to download a pdf with your budget statistics
 
 # IMPORTS
 import sys
+import os
 import commands                                         # Includes commands for above stated CLI commands
 import argparse
 import calendar
+import itertools
 from datetime import datetime
 
 class Budget():
-    def __init__(self, income_s, expense_s, month, year):
+    id_iter = itertools.count()
+    def __init__(self, name, income_s, expense_s, month, year):
+        self.id = next(self.id_iter)
+        self.name = name
         self.stable_incomes = income_s
         self.stable_expenses = expense_s
         self.current_month = month
         self.year = year
-        self.months = [month for month in calendar.month_name if month]
+        self.months = [month.lower() for month in calendar.month_name if month]
 
 # Exception hook
+# https://stackoverflow.com/questions/1319615/proper-way-to-declare-custom-exceptions-in-modern-python
 def exception_handler(exception_type, exception, traceback):
+
     if exception_type == Exception:
+        # TODO: Get error message from Exception Handler 
         print('ERROR: Please use correct syntax: --add <amount> <label>')
     else:
         sys.__excepthook__(exception_type, exception, traceback)
@@ -56,32 +71,50 @@ def main(args):
     # TODO Send args to command module function (acknowledge what command to use)
     # TODO Add more conditions and fix actions/placeholders
 
-
     if args.helpme:
         commands.show_commands()
     elif args.init:
-
-        # ------------------------------- CONTINUE ON THIS ------------------------------------
-
-        # Create budget framework (for specific years?)
-        # Setup labels for dynamic income and expenses
-        # Setup stable incomes dictionary {"CSN": 13000, "Tenant": 9300, "Salary": 4000} etc
-        # Setup stable expenses dictionary {"Rent": 3500, "Phone Bill": 500, "Spotify": 49} etc
+        # TODO: Make several budgets for each year (make it possible)
 
         year = datetime.now().year
         month = datetime.now().month
 
-        _income_label = set([x.replace(',', '').title() for x in input("Set INCOME labels (salary, loan etc): ").split()])
-        _expenses_label = set([x.replace(',', '').title() for x in input("Set EXPENSE labels ('rent, phone, spotify' etc): ").split()])
+        PATH = f"budgets/budget_{year}.json"
 
-        incomes = { label : input(f"{label}: ") for label in _income_label }
-        expenses = { label : input(f"{label}: ") for label in _expenses_label }
+        if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
+            # checks if file exists
+            answer = input(f"'/budget_{year}.json' already exists. Override? [y]/[n]: ")
+            if answer.lower() == "y":
+                print(f"'/budget_{year}.json' is about to be overriden. Press ctrl+C to cancel.")
+                budget_name = input(" >>> Name your new Budget: ")
 
-        budget = Budget(incomes, expenses, month, year)
-        commands.init(budget)
+                _income_label = set([x.replace(',', '').lower() for x in input(" >>> Set INCOME labels ('salary, loan' etc): ").split()])
+                _expenses_label = set([x.replace(',', '').lower() for x in input(" >>> Set EXPENSE labels ('rent, phone, spotify' etc): ").split()])
 
-    elif args.new:  # Add that you can decide if it's stable or dynamic income/expense label
-        print(f"Added label (placeholder): {args.new}")
+                incomes = { label : int(input(f" >>> Fixed income of {label}: ")) for label in _income_label }
+                expenses = { label : int(input(f" >>> Fixed expense of {label}: ")) for label in _expenses_label }
+
+
+                budget = Budget(budget_name, incomes, expenses, month, year)
+                commands.init(budget)
+            elif answer.lower() == "n":
+                print(f"'/budget_{year}.json' kept default values. Application terminated.")
+            else:
+                print("No matching input: Application terminated.")
+        else:
+            print("No file with that name exists.")
+    elif args.new:  
+        if len(args.new) != 2:
+            #raise Exception('Please use correct syntax: --new <category> <label>') 
+            print('ERROR: Please use correct syntax: --new <category> <label>')
+        else:
+            cat = args.new[0].lower()
+            lbl = args.new[1].lower()
+
+
+            commands.new(lbl, cat)
+
+
     elif args.delete:
         print(f"Removed label (placeholder): {args.delete}")
     elif args.show:
@@ -118,14 +151,13 @@ def main(args):
 
 
 if __name__ == "__main__":
-    # TODO Commands
 
     # TODO: add more arguments + fix arguments
     parser = argparse.ArgumentParser(description="Create a CLI Budget Scheme")
     parser.add_argument("--helpme", help="Shows all the argument options", action="store_true")   # Adds argument for "help" command
     parser.add_argument("--init", help="Initiates a new budget", action="store_true")
-    parser.add_argument("--new", help="Adds a new label to current budget")
-    parser.add_argument("--delete", help="Removes a label from current budget")
+    parser.add_argument("--new", nargs="*", help="Adds a new label to current budget")
+    parser.add_argument("--delete", nargs="*", help="Removes a label from current budget")
     parser.add_argument("--show", help="Shows budget as a pandas dataframe", action="store_true") 
     parser.add_argument("--labels", help="Shows all labels in current budget", action="store_true") 
     parser.add_argument("--add", nargs="*", help="Adds <amount> into <label> for current month/date/year") 
--- commands.py
+++ commands.py
@@ -1,7 +1,15 @@
-# TODO: När man sätter in värden, kolla datetime.now().month och lägg till på den månaden
 from budget import Budget
+from datetime import datetime
 import json
+import calendar
+import os
 
+MODES = ['FULL', 'YEAR', 'MONTH']
+MONTH = calendar.month_name[datetime.now().month].lower()
+ALL_MONTHS = [month.lower() for month in calendar.month_name if month]
+YEAR = datetime.now().year
+INCOME = "income"
+EXPENSE = "expense"
 
 # BUDGET FUNCTIONS
 def show_commands():
@@ -9,8 +17,8 @@ def show_commands():
     print("Showing all arguments for the Budget application: ")
     print("  --helpme                        Shows all the argument options")
     print("  --init                          Initiates a new budget with following commands")
-    print("  --new <labeL>                   Adds a new label to current budget")
-    print("  --delete <label>                Removes label from current budget")
+    print("  --new <category> <label>        Adds a new label to current budget in mentioned category")
+    print("  --delete <category> <label>     Removes label from current budget in mentioned category")
     print("  --show                          Shows budget as a pandas dataframe")
     print("  --labels                        Shos labels, ie Food, Snacks, Cinema, Taxis, Clothing")
     print("  --add <amount> <label>          Adds <amount> into <label> for current month/date/year")
@@ -18,44 +26,75 @@ def show_commands():
     print("  --undo                          Undos the latest command made")
 
 def init(Budget):
-    months = Budget.months
     current_budget = Budget
 
+    # Add name/year to budget?
     data = {
-        current_budget.year: {
-            month: {
-                "Fixed income": {
-                    label: value
-                    for label, value in current_budget.stable_incomes.items()
-                },
-                "Fixed expenses": {
-                    label: value
-                    for label, value in current_budget.stable_expenses.items()
-                }
+        month: {
+            INCOME: {
+                label: value
+                for label, value in current_budget.stable_incomes.items()
+            },
+            EXPENSE: {
+                label: value
+                for label, value in current_budget.stable_expenses.items()
             }
-            for month in months
         }
+        for month in current_budget.months
     }
 
-    with open(f'budgets/budget_{Budget.year}.json', 'w') as fp:
-        json.dump(data, fp,  indent=4)
+    with open(f'budgets/budget_{current_budget.year}.json', 'w', encoding='utf-8') as fp:
+        json.dump(data, fp, ensure_ascii=False, indent=4) 
 
-def new(label):
-    pass
+    print(f"Successfully created budget '{current_budget.name}'. (PATH '/budgets/budget_{current_budget.year}.json')")
+        
+
+def new(label, category):
+    with open(f"budgets/budget_2020.json", "r") as file:
+        data = json.load(file)
+    
+    if find_label(label, data, category):
+        print(f"A label with that name ({label}) already exists. ")
+        print(f"Use: '$ python3 budget.py manage <old_label> <new_label>' to change its name.")
+    else:
+        for m in ALL_MONTHS:
+            data[m][category][label] = 0
+        print(data[MONTH][category])
+
+        # TODO: Dynamic budget "budget_{current_budget.year}.json"
+        with open(f'budgets/budget_2020.json', 'w', encoding='utf-8') as fp:
+            json.dump(data, fp, ensure_ascii=False, indent=4) 
 
 def delete(label):
     pass
 
 def show(mode):
     # Check if mode is 'FULL', 'DAY', 'MONTH', 'YEAR'
-    pass
+    if mode not in MODES:
+        print("Can only show data from _ by year, month or full.")
+    else:
+        pass
 
-def add(amount, label):
+def add(amount, label):             
     """ Adds amount to specific label """
-    # if db.find_label(label):
-    #       add <amount> to label in json
+    with open(f"budgets/budget_2020.json", "r") as file:
+        data = json.load(file)
 
-    print("TBD execution of <def add()>")
+    category = find_category(label, data)
+
+    if find_label(label, data, category):
+        # FOUND LABEL
+        data[MONTH][category][label] += amount
+        print(f"Added {amount} SEK to {label} ({category}).")
+        print(f"Current {category} for {MONTH.title()}: {data[MONTH][category][label]}")
+
+        # TODO: Dynamic budget "budget_{current_budget.year}.json"
+        with open(f'budgets/budget_2020.json', 'w', encoding='utf-8') as fp:
+            json.dump(data, fp, ensure_ascii=False, indent=4)
+    else:
+        # DIDNT FIND LABEL
+        print(f"Couldn't find a label with name '{label}''. Did you spell right?")
+        print(f"Use: '$ python3 budget.py --new <label> <category (Income/Expense)>' to add a new label.")
 
 def sub(amount, label):
     """ Removes amount from specific label """
@@ -67,5 +106,33 @@ def undo():
     # Undo last command
     pass
 
+# OTHER FUNCTIONS
 
+def find(key, dictionary):
+    for k, v in dictionary.items():
+        if k == key:
+            yield dictionary
+        elif isinstance(v, dict):
+            for result in find(key, v):
+                yield result
+        elif isinstance(v, list):
+            for d in v:
+                if isinstance(d, dict):
+                    for result in find(key, d):
+                        yield result
 
+def find_label(label, data, category):
+    for key in data.keys():        
+        if key == MONTH:
+            return find_label(label, data[key], category)
+        elif key == category:
+            return find_label(label, data[key], category)
+        elif key == label:
+            return True
+
+def find_category(label, data):
+    for key in data.keys():        
+        if key == MONTH:
+            return find_category(label, data[key])
+        elif label in data[key]:
+            return key
